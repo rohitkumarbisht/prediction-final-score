ebf7ec88ad514d9e4199de10cd1729d437e508d7
Vaishali Agrawal <vaishali.agrawal@magicsw.com>
2023-09-01 15:21:11 +0530

    changed static form to dynamic form for all kinds of dataset

--- main.py
+++ main.py
@@ -8,24 +8,28 @@ model = None
 model_trained = False
 
 def perform_correlation_analysis(df, target_column):
-    correlation_with_G3 = df.corr()[target_column]
+    # Exclude the target column from the features
+    features = [col for col in df.columns if col != target_column]
+
+    correlation_with_G3 = df[features].corrwith(df[target_column])
 
     correlation_more_than_0_2 = [
-        col for col in correlation_with_G3.index
-        if 0.14 < correlation_with_G3[col]
+        col for col, corr in correlation_with_G3.items()
+        if 0.3 < corr
     ]
 
     correlation_less_than_minus_0_2 = [
-        col for col in correlation_with_G3.index
-        if correlation_with_G3[col] < -0.14
+        col for col, corr in correlation_with_G3.items()
+        if corr < -0.3
     ]
 
     correlation_minus_0_2_to_0_2 = [
-        col for col in correlation_with_G3.index
-        if -0.2 <= correlation_with_G3[col] <= 0.14
+        col for col, corr in correlation_with_G3.items()
+        if -0.3 <= corr <= 0.3
     ]
 
     return correlation_more_than_0_2, correlation_less_than_minus_0_2, correlation_minus_0_2_to_0_2
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -37,20 +41,18 @@ def index():
             model_trained = False
 
             df = pd.read_csv('uploaded.csv')
-            selected_features = ['age', 'failures', 'Medu', 'Fedu', 'G1', 'G2']
-            target_column = 'G3'
-            df_features = df[selected_features]
-            df_target = (df[target_column] >= 10).astype(int)
-
+            target_column = df.columns[-1]
             correlation_more_than_0_2, correlation_less_than_minus_0_2, correlation_minus_0_2_to_0_2 = perform_correlation_analysis(df, target_column)
 
+            selected_features = correlation_more_than_0_2 + correlation_less_than_minus_0_2 # Use highly correlated features for the form
+
             model = GradientBoostingClassifier()
-            model.fit(df_features, df_target)
+            model.fit(df[selected_features], (df[target_column] >= 10).astype(int))
 
             model_trained = True
 
             return render_template('index.html', file_uploaded=True, model_trained=model_trained,
-                                   corr_more_than_0_2=correlation_more_than_0_2,
+                                   selected_features=selected_features , corr_more_than_0_2=correlation_more_than_0_2,
                                    corr_less_than_minus_0_2=correlation_less_than_minus_0_2,
                                    corr_minus_0_2_to_0_2=correlation_minus_0_2_to_0_2)
     return render_template('index.html', file_uploaded=False, model_trained=model_trained)
@@ -58,20 +60,20 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    if not model_trained:
+        return render_template('index.html', file_uploaded=True, model_trained=False)
+
     # Get form inputs and prepare input data
-    age = int(request.form['age'])
-    failures = int(request.form['failures'])
-    medu = int(request.form['medu'])
-    fedu = int(request.form['fedu'])
-    g1 = int(request.form['g1'])
-    g2 = int(request.form['g2'])
-    input_data = [[age, failures, medu, fedu, g1, g2]]
+    selected_features = request.form.getlist('features')
+    input_data = []
+    for feature in selected_features:
+        input_data.append(int(request.form[feature]))
 
     # Make prediction
-    prediction = model.predict(input_data)
+    prediction = model.predict([input_data])
     result = "Pass" if prediction[0] == 1 else "Fail"
 
-    return render_template('index.html', prediction_result=result, file_uploaded=False, model_trained=False)
+    return render_template('index.html', prediction_result=result, file_uploaded=True, model_trained=model_trained, selected_features=selected_features)
 
 if __name__ == '__main__':
     app.run(debug=True)
