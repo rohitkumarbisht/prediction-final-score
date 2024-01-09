20009f7c580aa66dbe38349c0ccf944ed8cf3b3e
rohit.bisht <rohit.bisht@magicedtech.com>
2024-01-09 12:36:53 +0530

    removed not required files

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,7 +1,5 @@
-from flask import redirect, render_template, url_for
+from flask import redirect
 from flask_classful import FlaskView
-
-from app.utils.file_open import check_file_exists
 
 
 class Home(FlaskView):
